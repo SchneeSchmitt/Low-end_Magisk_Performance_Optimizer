69b5b8db946f0a4e3acb5a969264ec5572b0c45b
Snoocomics <157796309+SchneeSchmitt@users.noreply.github.com>
2025-07-12 11:42:43 +0800

    Add files via upload

---
+++ service.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+sleep 1
+
+# Renderer Configurations
+
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
